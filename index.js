5c43d7c75e38643af8cda1117ba27390b20b3a30
astera2.muhamad@gmail.com Muhammad Noori <astera2.muhamad@gmail.com>
2018-09-04 21:58:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,67 +1,101 @@
-// driver has many trips
-// passenger has many trips
-// trips
-//// passenger
-//// driver
+// global datastore
+let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
-let store = { drivers: [], passengers: [], trips: [] };
+const Neighborhood = (() => {
+  let neighborhoodIds = 1;
+  return class {
+    constructor(name) {
+      this.id = neighborhoodIds++;
+      this.name = name;
+      store.neighborhoods.push(this);
+    }
 
-let driverId = 0;
-let passengerId = 0;
-let tripId = 0;
+    customers() {
+      return store.customers.filter(customer => customer.neighborhoodId === this.id);
+    }
 
-class Driver {
-  constructor(name) {
-    this.name = name;
-    this.id = driverId++;
-    store.drivers.push(this);
-  }
-  trips() {
-    return store.trips.filter(trip => {
-      return trip.driverId == this.id;
-    });
-  }
-  passengers() {
-    return this.trips().map(trip => {
-      return trip.passenger();
-    });
-  }
-}
+    meals() {
+      const allMeals = this.customers().map(customer => customer.meals());
+      const merged = [].concat.apply([], allMeals);
+      return [...new Set(merged)];
+    }
 
-class Passenger {
-  constructor(name) {
-    this.name = name;
-    this.id = passengerId++;
-    store.passengers.push(this);
-  }
+    deliveries() {
+      return store.deliveries.filter(delivery => delivery.neighborhoodId === this.id);
+    }
+  };
+})();
 
-  trips() {
-    return store.trips.filter(trip => {
-      return trip.passengerId == this.id;
-    });
-  }
-  drivers() {
-    return this.trips().map(trip => {
-      return trip.driver();
-    });
-  }
-}
+const Meal = (() => {
+  let mealIds = 1;
+  return class {
+    constructor(title, price = 0) {
+      this.id = mealIds++;
+      this.title = title;
+      this.price = price;
+      store.meals.push(this);
+    }
 
-class Trip {
-  constructor(driver, passenger) {
-    this.driverId = driver.id;
-    this.passengerId = passenger.id;
-    this.id = tripId++;
-    store.trips.push(this);
-  }
-  driver() {
-    return store.drivers.find(driver => {
-      return driver.id === this.driverId;
-    });
-  }
-  passenger() {
-    return store.passengers.find(passenger => {
-      return passenger.id === this.passengerId;
-    });
-  }
-}
+    deliveries() {
+      return store.deliveries.filter(delivery => delivery.mealId === this.id);
+    }
+
+    customers() {
+      const allCustomers = this.deliveries().map(delivery => delivery.customer());
+      return [...new Set(allCustomers)];
+    }
+
+    static byPrice() {
+      return store.meals.sort((a, b) => a.price < b.price);
+    }
+  };
+})();
+
+const Customer = (() => {
+  let customerIds = 1;
+  return class {
+    constructor(name, neighborhoodId) {
+      this.id = customerIds++;
+      this.name = name;
+      this.neighborhoodId = neighborhoodId;
+      store.customers.push(this);
+    }
+
+    deliveries() {
+      return store.deliveries.filter(delivery => delivery.customerId === this.id);
+    }
+
+    meals() {
+      return this.deliveries().map(delivery => delivery.meal());
+    }
+
+    totalSpent() {
+      return this.meals().reduce((total, meal) => (total += meal.price), 0);
+    }
+  };
+})();
+
+const Delivery = (() => {
+  let deliveryIds = 1;
+  return class {
+    constructor(mealId, neighborhoodId, customerId) {
+      this.id = deliveryIds++;
+      this.mealId = mealId;
+      this.neighborhoodId = neighborhoodId;
+      this.customerId = customerId;
+      store.deliveries.push(this);
+    }
+
+    meal() {
+      return store.meals.find(meal => meal.id === this.mealId);
+    }
+
+    neighborhood() {
+      return store.neighborhoods.find(neighborhood => neighborhood.id === this.neighborhoodId);
+    }
+
+    customer() {
+      return store.customers.find(customer => customer.id === this.customerId);
+    }
+  };
+})();
